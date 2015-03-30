5312b334668a7826a7bd7d382200489dae33ee15
Sidney Ng <yendisng@gmail.com>
2015-03-30 15:09:28 -0400

    adding read/write/set high/low

--- RTX Base Project/inc/wireless.h
+++ RTX Base Project/inc/wireless.h
@@ -1,9 +1,45 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
 
+/* Generalize the file for transmitter and receiver on different boards */
+#define TRANSMITTER 0x00
+
+#define CC2500_SPI							SPI1
+#define CC2500_SPI_CLK					RCC_APB2Periph_SPI1
+
+#if TRANSMITTER
+/* Define the pins and ports for the configurations */
+#define CC2500_SPI_GPIO_CLK 		RCC_AHB1Periph_GPIOA
+#define CC2500_SPI_GPIO_PORT		GPIOA
+#define CC2500_SPI_GPIO_AF			GPIO_AF_SPI1
+
+#define CC2500_SPI_NSS_PIN    	GPIO_Pin_4
+#define CC2500_SPI_SCK_PIN    	GPIO_Pin_5
+#define CC2500_SPI_MISO_PIN    	GPIO_Pin_6
+#define CC2500_SPI_MOSI_PIN    	GPIO_Pin_7
+
+#else // they are the same ports nad pins, so the else can be deleted
+
+#define CC2500_SPI_GPIO_CLK 		RCC_AHB1Periph_GPIOA
+#define CC2500_SPI_GPIO_PORT		GPIOA
+#define CC2500_SPI_GPIO_AF			GPIO_AF_SPI1
+
+#define CC2500_SPI_NSS_PIN    	GPIO_Pin_4
+#define CC2500_SPI_SCK_PIN    	GPIO_Pin_5
+#define CC2500_SPI_MISO_PIN    	GPIO_Pin_6
+#define CC2500_SPI_MOSI_PIN    	GPIO_Pin_7
+
+#endif
+
+/* Define macros for general use */
+
 #define FLAG_TIMEOUT ((uint32_t)0x1000)
+
+#define CC2500_CS_LOW()       GPIO_ResetBits(CC2500_SPI_GPIO_PORT, CC2500_SPI_NSS_PIN)
+#define CC2500_CS_HIGH()      GPIO_SetBits(CC2500_SPI_GPIO_PORT, CC2500_SPI_NSS_PIN)
 
 static uint8_t SendByte(uint8_t byte);
 void wireless_Init();
 void wireless_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead);
 void wireless_Write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite);
+void Receive();
--- RTX Base Project/src/wireless.c
+++ RTX Base Project/src/wireless.c
@@ -1,6 +1,13 @@
 #include "wireless.h"
 
-uint32_t Timeout = FLAG_TIMEOUT;
+__IO uint32_t Timeout = FLAG_TIMEOUT;
+
+/* Read/Write command */
+#define READWRITE_CMD              ((uint8_t)0x80) 
+/* Multiple byte read/write command */ 
+#define MULTIPLEBYTE_CMD           ((uint8_t)0x40)
+/* Dummy Byte Send by the SPI Master device in order to generate the Clock to the Slave device */
+#define DUMMY_BYTE                 ((uint8_t)0x00)
 
 int TIMEOUT_UserCallback() {
 	while (1);
@@ -12,17 +19,17 @@ static void LowLevel_Init(void){
   SPI_InitTypeDef  SPI_InitStructure;
 
   // Enable the SPI periph
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+  RCC_APB2PeriphClockCmd(CC2500_SPI_CLK, ENABLE);
 
-  // Enable SCK, MOSI and MISO GPIO clocks
-	// PA5, PA7, PA6 */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  // Enable NSS, SCK, MOSI and MISO GPIO clocks
+	// PA4, PA5, PA7, PA6 */
+	RCC_AHB1PeriphClockCmd(CC2500_SPI_GPIO_CLK, ENABLE);
 	
 	// Alternate Functions
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1); // NSS
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1); // SCK
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1); // MISO
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1); // MOSI
+	GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_NSS_PIN, CC2500_SPI_GPIO_AF); // NSS - not slave select, similar to chip select
+  GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_SCK_PIN, CC2500_SPI_GPIO_AF); // SCK
+  GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_PIN, CC2500_SPI_GPIO_AF); // MISO
+	GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_MOSI_PIN, CC2500_SPI_GPIO_AF); // MOSI
 
   // Enable CS  GPIO clock
 	// use GPIOA again
@@ -34,24 +41,24 @@ static void LowLevel_Init(void){
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
   // SPI SCK pin configuration
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_SCK_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
   // SPI  MOSI pin configuration
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_MOSI_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
   //SPI MISO pin configuration
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_MISO_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 	
 	// NSS pin configuration
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = CC2500_SPI_NSS_PIN;
+	GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
   /* SPI configuration -------------------------------------------------------*/
 	// make sure you know why these are this way
-  SPI_I2S_DeInit(SPI1);
+  SPI_I2S_DeInit(CC2500_SPI);
   SPI_InitStructure.SPI_Direction 				= SPI_Direction_2Lines_FullDuplex;
   SPI_InitStructure.SPI_DataSize 					= SPI_DataSize_8b;
   SPI_InitStructure.SPI_CPOL 							= SPI_CPOL_Low; // polarity as per section 3.1 of configuring cc2500
@@ -61,20 +68,20 @@ static void LowLevel_Init(void){
   SPI_InitStructure.SPI_FirstBit 					= SPI_FirstBit_MSB;
   SPI_InitStructure.SPI_CRCPolynomial 		= 7;
   SPI_InitStructure.SPI_Mode 							= SPI_Mode_Master; // master mode as per section 3.1
-  SPI_Init(SPI1, &SPI_InitStructure);
+  SPI_Init(CC2500_SPI, &SPI_InitStructure);
 
   // Enable SPI1
-  SPI_Cmd(SPI1, ENABLE);
+  SPI_Cmd(CC2500_SPI, ENABLE);
 
   // Configure GPIO PIN for Lis Chip select
-  GPIO_InitStructure.GPIO_Pin 	= GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+//  GPIO_InitStructure.GPIO_Pin 	= GPIO_Pin_9;
+//  GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_OUT;
+//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
   // Deselect : Chip Select high
-  GPIO_SetBits(GPIOA, GPIO_Pin_9);
+  GPIO_SetBits(CC2500_SPI_GPIO_PORT, CC2500_SPI_NSS_PIN);
 }
 
 
@@ -83,7 +90,7 @@ static uint8_t SendByte(uint8_t byte) {
 	
 	// Loop while DR register is not empty
   Timeout = FLAG_TIMEOUT;
-	while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET){
+	while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_TXE) == RESET){
 		if((Timeout--) == 0) return TIMEOUT_UserCallback();
   }
   
@@ -92,26 +99,75 @@ static uint8_t SendByte(uint8_t byte) {
   
 	// Wait to receive a Byte
   Timeout = FLAG_TIMEOUT;
-  while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET){
+  while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_RXNE) == RESET){
     if((Timeout--) == 0) return TIMEOUT_UserCallback();
   }
   
   // Return the Byte read from the SPI bus
-  return (uint8_t)SPI_I2S_ReceiveData(SPI1);
+  return (uint8_t)SPI_I2S_ReceiveData(CC2500_SPI);
 }
 
 void wireless_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead) {
-	// TODO
+	if(NumByteToRead > 0x01)
+  {
+    ReadAddr |= (uint8_t)(READWRITE_CMD | MULTIPLEBYTE_CMD);
+  }
+  else
+  {
+    ReadAddr |= (uint8_t)READWRITE_CMD;
+  }
+  /* Set chip select Low at the start of the transmission */
+  CC2500_CS_LOW();
+  
+  /* Send the Address of the indexed register */
+  SendByte(ReadAddr);
+  
+  /* Receive the data that will be read from the device (MSB First) */
+  while(NumByteToRead > 0x00)
+  {
+    /* Send dummy byte (0x00) to generate the SPI clock to CC2500 (Slave device) */
+    *pBuffer = SendByte(DUMMY_BYTE);
+    NumByteToRead--;
+    pBuffer++;
+  }
+  
+  /* Set chip select High at the end of the transmission */ 
+  CC2500_CS_HIGH();
 }
 
 void wireless_Write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite) {
-	// TODO
+	  /* Configure the MS bit: 
+       - When 0, the address will remain unchanged in multiple read/write commands.
+       - When 1, the address will be auto incremented in multiple read/write commands.
+  */
+  if(NumByteToWrite > 0x01)
+  {
+    WriteAddr |= (uint8_t)MULTIPLEBYTE_CMD;
+  }
+  /* Set chip select Low at the start of the transmission */
+  CC2500_CS_LOW();
+  
+  /* Send the Address of the indexed register */
+  SendByte(WriteAddr);
+  /* Send the data that will be written into the device (MSB First) */
+  while(NumByteToWrite >= 0x01)
+  {
+    SendByte(*pBuffer);
+    NumByteToWrite--;
+    pBuffer++;
+  }
+  
+  /* Set chip select High at the end of the transmission */ 
+  CC2500_CS_HIGH();
 }
 
 void wireless_Init() {
 	uint8_t crtl1 = 0x00;
 	LowLevel_Init();
 	// TODO: control registers?
+	
+	// automatic power on
+	
 }
 
 
