0c8c61be797c7405ba3d60d84565bfebdceba2b7
Sidney Ng <yendisng@gmail.com>
2015-04-03 22:23:00 -0400

    adding the changes from the RTX project for wireless to the LCD

--- LCD Base Project/inc/wireless.h
+++ LCD Base Project/inc/wireless.h
@@ -1,23 +1,64 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
+#include <stdio.h>
 
-#define FLAG_TIMEOUT             ((uint32_t)0x1000)
+/* Generalize the file for transmitter and receiver on different boards */
 
-typedef struct
-{
-	uint8_t Power_Mode;
-	uint8_t Oscillator_Power;
-	uint8_t Receive_Transmit_Mode;
-	uint8_t RF_Channel;
-	uint8_t Data_Rate;
-	uint8_t Modulation_Format;
-	uint8_t RX_Channel_Filter_Bandwidth;
-	uint8_t RF_Output_Power;
-	uint8_t Data_Buffering;
-}CC2500_InitTypeDef;
+#define CC2500_SPI							SPI2
+#define CC2500_SPI_CLK					RCC_APB1Periph_SPI2
 
-void CC2500_Init(CC2500_InitTypeDef *CC2500_InitStruct);
-//void CC2500_InterruptConfig(CC2500_InterruptConfigTypeDef *CC2500_InterruptConfigStruct);
-//void CC2500_ReadMSG(int32_t* out);
-void CC2500_Write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite);
-void CC2500_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead);
+/* Define the pins and ports for the configurations */
+#define CC2500_SPI_GPIO_CLK 					RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_CS_GPIO_CLK				RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_GPIO_PORT					GPIOB
+#define CC2500_SPI_CS_GPIO_PORT				GPIOB
+#define CC2500_SPI_GPIO_AF						GPIO_AF_SPI2
+
+#define CC2500_SPI_NSS_PIN    				GPIO_Pin_12 // p17 
+#define CC2500_SPI_SCK_PIN    				GPIO_Pin_13 // p16
+#define CC2500_SPI_MISO_PIN    				GPIO_Pin_14 // p15
+#define CC2500_SPI_MOSI_PIN    				GPIO_Pin_15 // p18
+#define CC2500_SPI_NSS_SOURCE         GPIO_PinSource12
+#define CC2500_SPI_SCK_SOURCE         GPIO_PinSource13
+#define CC2500_SPI_MISO_SOURCE        GPIO_PinSource14
+#define CC2500_SPI_MOSI_SOURCE        GPIO_PinSource15
+
+
+#define SRES 0x30
+#define SFSTXON 0x31
+#define SXOFF 0x32
+#define SCAL 0x33
+#define SRX 0x34
+#define STX 0x35
+#define SIDLE 0x36
+#define SWOR 0x38
+#define SPWD 0x39
+#define SFRX 0x3A
+#define SFTX 0x3B
+#define SWORRST 0x3C
+#define SNOP 0x3D        
+
+/* Define macros for general use */
+
+#define FLAG_TIMEOUT ((uint32_t)0x1000)
+
+#define CC2500_CS_LOW()       GPIO_ResetBits(CC2500_SPI_CS_GPIO_PORT, CC2500_SPI_NSS_PIN)
+#define CC2500_CS_HIGH()      GPIO_SetBits(CC2500_SPI_CS_GPIO_PORT, CC2500_SPI_NSS_PIN)
+
+
+/*	Buffer status registers	*/
+#define CC2500REG_TXBYTES		(0x3A|0xFA)
+#define CC2500REG_RXBYTES		(0x3B|0xFB)
+
+/*	Burst access to FIFOs	*/
+#define CC2500REG_TX_FIFO		0x3F
+#define CC2500REG_RX_FIFO		0xBF
+
+static uint8_t SendByte(uint8_t byte);
+void wireless_Init(void);
+uint8_t CC2500_Strobe(uint8_t Strobe);
+void SPI_Read(uint8_t* pBuffer, uint8_t address, uint16_t bytesToRead);
+void SPI_Write(uint8_t* pBuffer, uint8_t address, uint16_t bytesToWrite);
+void ReadRecvBuffer(uint8_t *buffer);
+void Transmit(uint8_t *buffer);
+void delay(long num_ticks);
--- LCD Base Project/src/wireless.c
+++ LCD Base Project/src/wireless.c
@@ -1,122 +1,361 @@
 #include "wireless.h"
-
-static uint8_t SendByte(uint8_t byte);
-static void LowLevel_Init(void);
+#include "cc2500_settings.h"
 
 __IO uint32_t Timeout = FLAG_TIMEOUT;
 
-TIMEOUT_UserCallback() {
+int TIMEOUT_UserCallback(void) {
 	while (1);
 }
 
-void CC2500_Init(CC2500_InitTypeDef *CC2500_InitStruct) {
-	uint8_t crtl1 = 0x00;
-	
-	LowLevel_Init();
-	
-	// control registers?
+uint8_t CC2500_state;
+
+void delay(long num_ticks){
+	while(num_ticks-- > 0);
 }
 
-void CC2500_Write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite) {
-	
+uint8_t CC2500_Strobe(uint8_t Strobe){
+	CC2500_CS_LOW();
+
+	while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_TXE)== RESET);	// check flag for transmission to be RESET
+	SPI_I2S_SendData(CC2500_SPI, Strobe);												// condition satisfied --> send command strobe
+
+	while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_RXNE) == RESET);		// check flag for being busy to be SET
+	CC2500_state = (SPI_I2S_ReceiveData(CC2500_SPI) & 0x70) >> 4;											// set status to most recent received data on SPI1
+
+	// Set chip select High at the end of the transmission
+	CC2500_CS_HIGH();   
+	return CC2500_state;
 }
 
-void CC2500_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead) {
-	
-}
-
-static uint8_t SendByte(uint8_t byte) {
-	 /* Loop while DR register in not empty */
-  Timeout = FLAG_TIMEOUT;
-  while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
-  {
-    if((Timeout--) == 0) return TIMEOUT_UserCallback();
-  }
+// Initializes the low level interface used to drive the wireless components
+static void LowLevel_Init(void){
   
-  /* Send a Byte through the SPI peripheral */
-  SPI_I2S_SendData(L3GD20_SPI, (uint16_t)byte);
-  /* Wait to receive a Byte */
-  Timeout = FLAG_TIMEOUT;
-  while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
-  {
-    if((Timeout--) == 0) return TIMEOUT_UserCallback();
-  }
-  
-  /* Return the Byte read from the SPI bus */
-  return (uint8_t)SPI_I2S_ReceiveData(SPI1);
-}
-
-/**
-  * @brief  Initializes the low level interface used to drive the wireless components
-  * @param  None
-  * @retval None
-  */
-static void LowLevel_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
   SPI_InitTypeDef  SPI_InitStructure;
-	
-  /* Enable the SPI periph */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
-  /* Enable SCK, MOSI and MISO GPIO clocks */
-	/* PA5, PA7, PA6 */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  // Enable the SPI periph
+  RCC_APB1PeriphClockCmd(CC2500_SPI_CLK, ENABLE);
+  
+	// Enable NSS, SCK, MOSI and MISO GPIO clocks
+	RCC_AHB1PeriphClockCmd(CC2500_SPI_GPIO_CLK, ENABLE);
 	
-	/* Alternate Functions */
-	//GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1); // NSS
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1); // SCK
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1); // MISO
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1); // MOSI
+	// Enable CSn clock
+	//RCC_AHB1PeriphClockCmd(CC2500_SPI_CS_GPIO_CLK, ENABLE);
+	
+	// Alternate Functions
+  GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_SCK_SOURCE, CC2500_SPI_GPIO_AF); // SCK
+  GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_SOURCE, CC2500_SPI_GPIO_AF); // MISO
+	GPIO_PinAFConfig(CC2500_SPI_GPIO_PORT, CC2500_SPI_MOSI_SOURCE, CC2500_SPI_GPIO_AF); // MOSI
 
-  /* Enable CS  GPIO clock */ // use GPIOA again
-  //RCC_AHB1PeriphClockCmd(LIS302DL_SPI_CS_GPIO_CLK, ENABLE);
-	
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-  /* SPI SCK pin configuration */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  // SPI SCK pin configuration
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_SCK_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
-  /* SPI  MOSI pin configuration */
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_7;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  // SPI  MOSI pin configuration
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_MOSI_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
-  /* SPI MISO pin configuration */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	/* NSS */
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-//  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  //SPI MISO pin configuration
+  GPIO_InitStructure.GPIO_Pin = CC2500_SPI_MISO_PIN;
+  GPIO_Init(CC2500_SPI_GPIO_PORT, &GPIO_InitStructure);
 
   /* SPI configuration -------------------------------------------------------*/
 	// make sure you know why these are this way
-  SPI_I2S_DeInit(SPI1);
-  SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-  SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-  SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low; // as per section 3.1 of configuring cc2500
-  SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-  SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-  SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
-  SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-  SPI_InitStructure.SPI_CRCPolynomial = 7;
-  SPI_InitStructure.SPI_Mode = SPI_Mode_Master; // master mode as per section 3.1
-  SPI_Init(SPI1, &SPI_InitStructure);
+  SPI_I2S_DeInit(CC2500_SPI);
+  SPI_InitStructure.SPI_Direction 				= SPI_Direction_2Lines_FullDuplex;
+  SPI_InitStructure.SPI_DataSize 					= SPI_DataSize_8b;
+  SPI_InitStructure.SPI_CPOL 							= SPI_CPOL_Low; // polarity as per section 3.1 of configuring cc2500
+  SPI_InitStructure.SPI_CPHA 							= SPI_CPHA_1Edge; // phase as per section 3.1 of configuring cc2500
+  SPI_InitStructure.SPI_NSS 							= SPI_NSS_Soft;
+  SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+  SPI_InitStructure.SPI_FirstBit 					= SPI_FirstBit_MSB;
+  SPI_InitStructure.SPI_CRCPolynomial 		= 7;
+  SPI_InitStructure.SPI_Mode 							= SPI_Mode_Master; // master mode as per section 3.1
+  SPI_Init(CC2500_SPI, &SPI_InitStructure);
 
-  /* Enable SPI1  */
-  SPI_Cmd(SPI1, ENABLE);
+  // Enable SPI1
+  SPI_Cmd(CC2500_SPI, ENABLE);
 
-  /* Configure GPIO PIN for Lis Chip select */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  // Configure GPIO PIN for Lis Chip select
+  GPIO_InitStructure.GPIO_Pin 	= CC2500_SPI_NSS_PIN;
+  GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(CC2500_SPI_CS_GPIO_PORT, &GPIO_InitStructure);
 
-  /* Deselect : Chip Select high */
-  GPIO_SetBits(GPIOA, GPIO_Pin_9);
+  // Deselect : Chip Select high
+  GPIO_SetBits(CC2500_SPI_CS_GPIO_PORT, CC2500_SPI_NSS_PIN);
+	
+	CC2500_CS_LOW();
+	delay(100);
+	CC2500_CS_HIGH();
+	delay(100);
+	CC2500_CS_LOW();
+	delay(150);
+	
+	// Send reset command
+	CC2500_Strobe(SRES); 
+	
+	while ((GPIO_ReadInputDataBit(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_PIN) & 0x80) == 0x80);
+	//while(!GPIO_ReadInputDataBit(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_PIN));
+	
+	// Set to IDLE state
+	uint8_t state = CC2500_Strobe(SIDLE);
+	
+	while (state != 0x00) {
+		state = CC2500_Strobe(SNOP);
+	}
+	
+	CC2500_CS_HIGH();
 }
+
+
+// Sends a byte through SPI protocol
+static uint8_t SendByte(uint8_t byte) {
+	
+	// Loop while DR register is not empty
+  Timeout = FLAG_TIMEOUT;
+	while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_TXE) == RESET){
+		if((Timeout--) == 0) return TIMEOUT_UserCallback();
+  }
+  
+  // Send a Byte through the SPI peripheral
+  SPI_I2S_SendData(CC2500_SPI, byte);
+  
+	// Wait to receive a Byte
+  Timeout = FLAG_TIMEOUT;
+  while (SPI_I2S_GetFlagStatus(CC2500_SPI, SPI_I2S_FLAG_RXNE) == RESET){
+    if((Timeout--) == 0) return TIMEOUT_UserCallback();
+  }
+  
+  // Return the Byte read from the SPI bus
+  return (uint8_t)SPI_I2S_ReceiveData(CC2500_SPI);
+}
+
+void SPI_Read(uint8_t* pBuffer, uint8_t address, uint16_t bytesToRead) {
+	
+	// enable Read mode
+	uint8_t RW_mode = 0x80;
+	
+	// if more than 1 byte to read, enable burst mode
+	uint8_t burst_mode = 0x00;
+	if (bytesToRead > 0x01){
+		burst_mode = 0x40;
+	}
+	
+	// concatenate R/W and Burst bits to address
+	address = RW_mode | burst_mode | address;
+  //printf ("Read Add: %i \n", address);
+	// start SPI
+	CC2500_CS_LOW();
+	
+	// send desired source address of read
+	SendByte(address);
+	
+	// for each byte to read, send a dummy byte through SPI
+	// send byte returned by SPI to buffer
+	while (bytesToRead > 0x00){
+		*pBuffer = SendByte (0x00);
+		//printf("Value read: %i\n", *pBuffer);
+		pBuffer ++;
+		bytesToRead--;
+	}
+		
+	// end SPI
+	CC2500_CS_HIGH();
+}
+
+void SPI_Write(uint8_t* pBuffer, uint8_t address, uint16_t bytesToWrite) {
+			
+	// if more than 1 byte to write, enable burst mode
+	uint8_t burst_mode = 0x00;
+	if (bytesToWrite > 0x01){
+		burst_mode = 0x40;
+	}
+	
+	//printf("bytes to write: %i\n", bytesToWrite);
+	
+	// concatenate Burst bit to address
+	address = burst_mode | address;
+	//printf ("Write Add: %i \n", address);
+	// start SPI
+	CC2500_CS_LOW();
+	
+	// send desired destination address of write
+	SendByte(address);
+	
+	// send each byte in the buffer to registers through SPI
+	while (bytesToWrite > 0x00){
+		//printf("Value to write: %i\n", *pBuffer);
+		SendByte (*pBuffer);
+		pBuffer ++;
+		bytesToWrite--;
+	}
+		
+	// end SPI
+	CC2500_CS_HIGH();
+}
+
+void Transmit(uint8_t *buffer) {
+	
+}
+
+void ReadRecvBuffer(uint8_t *buffer) {
+	
+	CC2500_CS_LOW();
+	
+	uint8_t current_status; 
+	uint8_t data_received; 
+	current_status = CC2500_Strobe(SIDLE);								
+	
+	while (current_status != 0x00){
+		current_status = CC2500_Strobe(SNOP);
+	}
+	
+	current_status = CC2500_Strobe(SRX);
+	
+	while (current_status != 0x01){
+		current_status = CC2500_Strobe(SNOP);
+	}
+
+	while (current_status == 0x01){
+		uint8_t NumBytesinFIFO = 0x08;
+		SPI_Read(&NumBytesinFIFO, CC2500REG_RXBYTES, 0x02);
+		if (NumBytesinFIFO >= 0x01){
+			//printf ("#bytes: 0x%02x\n", NumBytesinFIFO);
+			SPI_Read(&data_received, CC2500REG_RX_FIFO, 0x01);
+			printf ("data: 0x%02x\n", data_received);
+		}
+		delay(100);
+		current_status = CC2500_Strobe(SNOP);
+	}
+	
+	
+	
+	
+//	CC2500_CS_LOW();
+//	
+//	CC2500_Strobe(SRX);
+//	
+//	while ((GPIO_ReadInputDataBit(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_PIN) & 0x80) == 0x80);
+//	
+//	uint8_t bytes_avail = CC2500_Strobe(SNOP) & 0x0F;
+//	
+//	if (bytes_avail > 0) {
+//		if (bytes_avail > 1) {
+//			SPI_Read(buffer, 0xBF, bytes_avail);
+//			printf("received: %i\n", buffer[0]);
+//		} else {
+//			SPI_Read(buffer, 0xFF, bytes_avail);
+//			printf("received: %i\n", buffer[0]);
+//		}
+//	}
+//	
+//	CC2500_Strobe(SIDLE);
+//	
+//	while ((GPIO_ReadInputDataBit(CC2500_SPI_GPIO_PORT, CC2500_SPI_MISO_PIN) & 0x80) == 0x80);
+//	
+//	CC2500_Strobe(SFRX);
+		
+	CC2500_CS_HIGH();
+}
+
+void wireless_Init(void) {
+	LowLevel_Init();
+	
+	// Settings
+	uint8_t w_buffer[1];
+
+	w_buffer[0] = VAL_CC2500_IOCFG2;
+	SPI_Write(&w_buffer[0], 0x00, 1);
+
+	w_buffer[0] = VAL_CC2500_IOCFG0;
+	SPI_Write(&w_buffer[0], 0x02, 1);
+	
+	w_buffer[0] = VAL_CC2500_FIFOTHR;
+	SPI_Write(&w_buffer[0], 0x03, 1);
+
+	w_buffer[0] = VAL_CC2500_PKTLEN;
+	SPI_Write(&w_buffer[0], 0x06, 1);
+	w_buffer[0] = VAL_CC2500_PKTCTRL1;
+	SPI_Write(&w_buffer[0], 0x07, 1);
+	w_buffer[0] = VAL_CC2500_PKTCTRL0;
+	SPI_Write(&w_buffer[0], 0x08, 1);
+	w_buffer[0] = VAL_CC2500_ADDR;
+	SPI_Write(&w_buffer[0], 0x09, 1);
+	
+	w_buffer[0] = VAL_CC2500_CHANNR;
+	SPI_Write(&w_buffer[0], 0x0A, 1);
+	w_buffer[0] = VAL_CC2500_FSCTRL1;
+	SPI_Write(&w_buffer[0], 0x0B, 1);
+	w_buffer[0] = VAL_CC2500_FSCTRL0;
+	SPI_Write(&w_buffer[0], 0x0C, 1);
+	w_buffer[0] = VAL_CC2500_FREQ2;
+	SPI_Write(&w_buffer[0], 0x0D, 1);
+	w_buffer[0] = VAL_CC2500_FREQ1;
+	SPI_Write(&w_buffer[0], 0x0E, 1);
+	
+	w_buffer[0] = VAL_CC2500_FREQ0;
+	SPI_Write(&w_buffer[0], 0x0F, 1);
+	w_buffer[0] = VAL_CC2500_MDMCFG4;
+	SPI_Write(&w_buffer[0], 0x10, 1);
+	w_buffer[0] = VAL_CC2500_MDMCFG3;
+	SPI_Write(&w_buffer[0], 0x11, 1);
+	w_buffer[0] = VAL_CC2500_MDMCFG2;
+	SPI_Write(&w_buffer[0], 0x12, 1);
+	w_buffer[0] = VAL_CC2500_MDMCFG1;
+	SPI_Write(&w_buffer[0], 0x13, 1);
+	
+	w_buffer[0] = VAL_CC2500_MDMCFG0;
+	SPI_Write(&w_buffer[0], 0x14, 1);
+	w_buffer[0] = VAL_CC2500_DEVIATN;
+	SPI_Write(&w_buffer[0], 0x15, 1);
+	w_buffer[0] = 0x00;
+	w_buffer[0] = VAL_CC2500_MCSM1;
+	SPI_Write(&w_buffer[0], 0x17, 1);
+	w_buffer[0] = VAL_CC2500_MCSM0;
+	SPI_Write(&w_buffer[0], 0x18, 1);
+	
+	w_buffer[0] = VAL_CC2500_FOCCFG;
+	SPI_Write(&w_buffer[0], 0x19, 1);
+	w_buffer[0] = VAL_CC2500_BSCFG;
+	SPI_Write(&w_buffer[0], 0x1A, 1);
+	w_buffer[0] = VAL_CC2500_AGCTRL2;
+	SPI_Write(&w_buffer[0], 0x1B, 1);
+	w_buffer[0] = VAL_CC2500_AGCTRL1;
+	SPI_Write(&w_buffer[0], 0x1C, 1);
+	w_buffer[0] = VAL_CC2500_AGCTRL0;
+	SPI_Write(&w_buffer[0], 0x1D, 1);
+
+	w_buffer[0] = VAL_CC2500_FREND1;
+	SPI_Write(&w_buffer[0], 0x21, 1);
+	w_buffer[0] = VAL_CC2500_FREND0;
+	SPI_Write(&w_buffer[0], 0x22, 1);
+	
+	w_buffer[0] = VAL_CC2500_FSCAL3;
+	SPI_Write(&w_buffer[0], 0x23, 1);
+	w_buffer[0] = VAL_CC2500_FSCAL2;
+	SPI_Write(&w_buffer[0], 0x24, 1);
+	w_buffer[0] = VAL_CC2500_FSCAL1;
+	SPI_Write(&w_buffer[0], 0x25, 1);
+	w_buffer[0] = VAL_CC2500_FSCAL0;
+	SPI_Write(&w_buffer[0], 0x26, 1);
+
+	w_buffer[0] = VAL_CC2500_FSTEST;
+	SPI_Write(&w_buffer[0], 0x29, 1);
+
+	w_buffer[0] = VAL_CC2500_TEST2;
+	SPI_Write(&w_buffer[0], 0x2C, 1);
+	
+	w_buffer[0] = VAL_CC2500_TEST1;
+	SPI_Write(&w_buffer[0], 0x2D, 1);
+	w_buffer[0] = VAL_CC2500_TEST0;
+	SPI_Write(&w_buffer[0], 0x2E, 1);
+}
+
+
