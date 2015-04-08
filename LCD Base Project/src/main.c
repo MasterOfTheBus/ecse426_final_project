554c710160d67f97f5e2c51f5faed2e3cc584306
unknown <sho35@156TR4160MP-21.campus.MCGILL.CA>
2015-04-08 18:53:13 -0400

    LCD

--- LCD Base Project/src/main.c
+++ LCD Base Project/src/main.c
@@ -14,6 +14,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "LCD.h"
+#include "math.h"
+
+#define PI 3.14159265359
+int direction;
+int mode;
+int shape;
+int send;
+
+uint16_t x1;
+uint16_t y1;
+uint16_t x2;
+uint8_t y2;
 
 
 static void delay(__IO uint32_t nCount)
@@ -33,6 +46,16 @@ static void delay(__IO uint32_t nCount)
   * @param    None
   * @retval   None
   */
+
+// ID for theads
+osThreadId example_1a_thread_id;
+osThreadId example_1b_thread_id;
+osThreadId example_1c_thread_id;
+osThreadId square_thread_id;
+osThreadId rectangle_thread_id;
+osThreadId triangle_thread_id;
+osThreadId line_thread_id;
+osThreadId lcd_drawing_thread_id;
 
 void example_1a(void const *argument){
 	while(1){
@@ -176,20 +199,119 @@ void example_1c(void const *argument){
 		osDelay(250);
 	}
 }
+	
+void square(void const *argument){
+	while(1){
+		printf("square");
+		osSignalWait(0x01,osWaitForever);
+	  LCD_DrawFullCircle(120, 160, 50);
+		LCD_SetTextColor(LCD_COLOR_BLUE2); 
+		
+	 
+		osDelay(50);
+	}
+}
+void rectangle(void const *argument){
+	while(1){
+		printf("rectangle");
+		osSignalWait(0x01,osWaitForever);
+		LCD_DrawRect(90,130,60,60);
+		LCD_SetTextColor(LCD_COLOR_MAGENTA);		
+		
+		osDelay(50);
+	}
+}
+
+void triangle(void const *argument){
+	while(1){
+		printf("triangle");
+		osSignalWait(0x01,osWaitForever);
+		LCD_FillTriangle(90, 120, 150, 130, 180, 130);
+		LCD_SetTextColor(LCD_COLOR_RED);
+
+		
+		osDelay(50);
+	}
+}
+
+void line(void const *argument){
+	while(1){
+		printf("line");
+		osSignalWait(0x01,osWaitForever);
+		LCD_SetTextColor(LCD_COLOR_RED);
+		//LCD_DrawLine(uint16_t Xpos, uint16_t Ypos, uint16_t Length, uint8_t Direction);
+		//LCD_DrawUniLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+		LCD_DrawUniLine(x1,y1,x2,y2);
+		
+		osDelay(50);
+		}
+	}
+
+	
+void lcd_drawing_thread(void const *arguemnt){
+	
+while(1){
+
+	lcd_drawing();
+	//printf("mode: %i \ndirection: %i \nshape: %i", mode, direction, shape );
+	if (send == 1){
+			if (shape == 0 && direction == RESET){ // move to bottom left corner
+				LCD_Clear(LCD_COLOR_WHITE);
+				x1=0;
+				x2=0;
+				y1=0;
+				y2=0;
+			}
+
+		if(mode == STAR){
+	
+				if (shape == SQUARE){
+					
+				 osSignalSet(square_thread_id, 0x01);
+					
+				} else if (shape == RECTANGLE){
+						 osSignalSet(rectangle_thread_id, 0x01);
+				}
+					else if (shape == TRIANGLE){
+						osSignalSet(triangle_thread_id, 0x01);
+				}
+					else if (shape ==0){
+						x1=x2;
+						x2=x2+10*cos(PI*direction/180);
+						y1=y2;
+						y2=y2+10*sin(PI*direction/180);
+						osSignalSet(line_thread_id, 0x01);
+						
+
+						
+				
+					}
+				}
+			}
+		//printf("before delay");
+		osDelay(250);
+		}
+	
+}
+	
 
 osThreadDef(example_1a, osPriorityNormal, 1, 0);
 osThreadDef(example_1b, osPriorityNormal, 1, 0);
 osThreadDef(example_1c, osPriorityNormal, 1, 0);
+osThreadDef(square, osPriorityNormal, 1, 0);
+osThreadDef(rectangle, osPriorityNormal, 1, 0);
+osThreadDef(triangle, osPriorityNormal, 1, 0);
+osThreadDef(line, osPriorityNormal, 1, 0);
+osThreadDef(lcd_drawing_thread,osPriorityNormal, 1,0);
 
-// ID for theads
-osThreadId example_1a_thread;
-osThreadId example_1b_thread;
-osThreadId example_1c_thread;
+
+
 
 /*
  * main: initialize and start the system
  */
 int main (void) {
+	
   osKernelInitialize ();                    // initialize CMSIS-RTOS
 	
   // initialize peripherals here
@@ -204,8 +326,15 @@ int main (void) {
   
   /* Set LCD foreground layer as the current layer */
   LCD_SetLayer(LCD_FOREGROUND_LAYER);
+	LCD_Clear(LCD_COLOR_WHITE);
 	
-	
+	lcd_drawing_thread_id=osThreadCreate(osThread(lcd_drawing_thread), NULL);
+	square_thread_id = osThreadCreate(osThread(square), NULL);
+	rectangle_thread_id = osThreadCreate(osThread(rectangle), NULL);
+	triangle_thread_id = osThreadCreate(osThread(triangle), NULL);
+	line_thread_id = osThreadCreate(osThread(line), NULL);
+		
+
 	
   // create 'thread' functions that start executing,
   // example: tid_name = osThreadCreate (osThread(name), NULL);
@@ -223,5 +352,6 @@ int main (void) {
 	
 	osKernelStart ();                         // start thread execution 
 }
+				
 
 
