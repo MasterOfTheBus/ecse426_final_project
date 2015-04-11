b65a629a355db127f87a082ac35ce05526ee99d3
Sidney Ng <yendisng@gmail.com>
2015-04-10 21:47:21 -0400

    better receive implementation that is more consistent

--- RTX Base Project/src/main.c
+++ RTX Base Project/src/main.c
@@ -25,30 +25,47 @@ int motor_2_angle;
 void ReceiveData(void const *argument){
 	uint8_t r_buffer;
 	uint8_t prev;
+	uint8_t not_idle = 1;
 	while (1){
-		prev = r_buffer;
-		CC2500_Change_State (SRX);
-		osSignalWait(RECVSIG, osWaitForever);
-		CC2500_ReadRecvBuffer(&r_buffer);
+		if (not_idle) {
+			prev = r_buffer;
+			CC2500_Change_State (SRX);
+			osSignalWait(RECVSIG, osWaitForever);
+		}
 		uint8_t status = status_state(CC2500_Strobe(SNOP));
-		if (r_buffer != prev && status != RX_STATE) {
-			// processing for non-repeated data
-			printf("read: %i\n", r_buffer);
+		if (status != IDLE_STATE) {
+			not_idle = 0;
+			osDelay(100);
+		} else {
+			not_idle = 1;
+		}
+		if (not_idle) {
+			not_idle = 1;
+			CC2500_ReadRecvBuffer(&r_buffer);
+			if (r_buffer != prev && status != RX_STATE) {
+				// processing for non-repeated data
+				printf("read: 0x%02x\n", r_buffer);
+#if 0
+				// send a confirmation packet to the sender
+				uint8_t pkt = CONFIRMATION_PKT;
+				CC2500_Transmit(&pkt, 1);
+#endif
+			}
 		}
 	}
 }
 
 osThreadDef(ReceiveData, osPriorityNormal, 1, 0);
 
-osThreadDef(motor_0_thread, osPriorityNormal, 1, 0);
-osThreadDef(motor_2_thread, osPriorityNormal, 1, 0);
-osThreadDef(motor_1_thread, osPriorityNormal, 1, 0);
+//osThreadDef(motor_0_thread, osPriorityNormal, 1, 0);
+//osThreadDef(motor_2_thread, osPriorityNormal, 1, 0);
+//osThreadDef(motor_1_thread, osPriorityNormal, 1, 0);
 
 // ID for thread
 osThreadId RecvData_thread;
-osThreadId motor_0_thread_id;
-osThreadId motor_1_thread_id;
-osThreadId motor_2_thread_id;
+//osThreadId motor_0_thread_id;
+//osThreadId motor_1_thread_id;
+//osThreadId motor_2_thread_id;
 
 
 /*
@@ -87,9 +104,9 @@ void TIM3_IRQHandler(void)
 	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
 	{
 		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-		osSignalSet(motor_0_thread_id, 0x01);
-		osSignalSet(motor_1_thread_id, 0x01);
-		osSignalSet(motor_2_thread_id, 0x01);
+//		osSignalSet(motor_0_thread_id, 0x01);
+//		osSignalSet(motor_1_thread_id, 0x01);
+//		osSignalSet(motor_2_thread_id, 0x01);
 	}
 }
 
